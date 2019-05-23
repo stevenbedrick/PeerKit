c0957036b4ae8f82289c2ebfd67f3a9107e6e513
Steven Bedrick <steve@bedrick.org>
2019-05-23 10:23:22 -0700

    Auto-accept!

--- PeerKit/Advertiser.swift
+++ PeerKit/Advertiser.swift
@@ -40,7 +40,12 @@ class Advertiser: NSObject, MCNearbyServiceAdvertiserDelegate {
     func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
 
         // keep us from accepting our own invitation?
-        let accept = mcSession.myPeerID.displayName.hashValue != peerID.displayName.hashValue
+        print("PEERKIT debug: in didReceiveInvitation fromPeer: \(peerID)")
+        print("PEERKIT debug: myPeer displayName: \(mcSession.myPeerID.displayName), hashValue: \(mcSession.myPeerID.displayName.hashValue), raw hash value: \(mcSession.myPeerID.hashValue)")
+        print("PEERKIT debug: their peer displayName: \(peerID.displayName), hashValue: \(peerID.displayName.hashValue), raw hash value: \(mcSession.myPeerID.hashValue)")
+        
+//        let accept = mcSession.myPeerID.displayName.hashValue != peerID.displayName.hashValue
+        let accept = true
         
         invitationHandler(accept, mcSession)
         if accept {
